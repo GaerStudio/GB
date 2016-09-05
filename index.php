59b0a5d9e3de3fc5f4e861f414fcc6533f1c902d
GaerStudio <gaerstudio@mail.ru>
2016-09-05 15:13:33 +0300

    Обновленная главная страница

--- index.php
+++ index.php
@@ -1,7 +1,32 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Gaer
- * Date: 05.09.16
- * Time: 13:51
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title> GaerStudio </title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+    <div>
+        <div class="title"> GaerStudio <sup> &reg; </sup> <img src="img/Gaer.png" height="128" alt="GaerStudio"> </div>
+            <hr>
+        <div class="body">
+            <table>
+                <tr>
+                    <td> ЛОГИН </td>
+                    <td> <input type="text" name="login"> </td>
+                    <td rowspan="3"> <font color="red"> На сайте ведутся работы, регистрация новых пользователей временно приостановлена, ориентировачное время запуска сайта ..... </font> </td>
+                </tr>
+                <tr>
+                    <td> ПАРОЛЬ </td>
+                    <td> <input type="password" name="pass"> </td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="right"> <input type="submit" value="ВХОД"> </td>
+                </tr>
+            </table>
+        </div>
+            <hr>
+        <div class="footer"> GaerStudio &copy; 2016 </div>
+    </div>
+</body>
+</html>
